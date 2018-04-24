a141daab671539ac03e94af7ddcbe4d81a208cdb
Daryl Haines <daryl@southeastpixels.com>
2018-04-24 11:15:46 +0000

    Add admin user funcitonality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :articles
+  has_many :articles, dependent: :destroy # destroys all articles created by this user when the user account is deleted
   before_save { self.email = email.downcase }
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
